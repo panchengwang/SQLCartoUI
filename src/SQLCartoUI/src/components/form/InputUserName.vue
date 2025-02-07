6a1744bad8684a42ead1b4e3506d6c8c1f89c0c1
pcwang （麓山老将） <wang_wang_lao@163.com>
2025-02-07 22:16:57 +0800

    Create InputUserName.vue

---
+++ src/SQLCartoUI/src/components/form/InputUserName.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-input
+    dense
+    outlined
+    hide-bottom-space
+    type="text"
+    label="Input email as user name"
+    :rules="[
+      (val) =>
+        /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(val.toLowerCase()) ||
+        'Email is invalid! Please check it!',
+    ]"
+  >
+    <template v-slot:append>
+      <q-btn color="primary" dense round size="sm" icon="how_to_reg" no-caps>
+        <q-tooltip class="bg-accent" anchor="top middle">Check if user exists</q-tooltip>
+      </q-btn>
+    </template>
+  </q-input>
+</template>
+
+<script setup></script>
