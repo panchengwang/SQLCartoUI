1d0c2b70ef33b773455cc45133e21dab5e8e95f1
pcwang （麓山老将） <wang_wang_lao@163.com>
2025-02-07 22:16:51 +0800

    Create InputCaptcha.vue

---
+++ src/SQLCartoUI/src/components/form/InputCaptcha.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-input
+    dense
+    outlined
+    hide-bottom-space
+    type="text"
+    label="Input Captcha"
+    :rules="[
+      (val) =>
+        (val && val.length === 8) ||
+        'The password does not match the one entered previously.',
+    ]"
+  >
+    <template v-slot:append>
+      <q-btn dense size="sm" round color="primary" icon="code" no-caps>
+        <q-tooltip class="bg-accent" anchor="top middle">Get captcha</q-tooltip>
+      </q-btn>
+    </template>
+  </q-input>
+</template>
+<script setup></script>
