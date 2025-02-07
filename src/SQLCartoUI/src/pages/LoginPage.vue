d78836bbeb58d056e3b57060c49c0a33560d45e8
pcwang （麓山老将） <wang_wang_lao@163.com>
2025-02-07 23:10:30 +0800

    Update LoginPage.vue

--- src/SQLCartoUI/src/pages/LoginPage.vue
+++ src/SQLCartoUI/src/pages/LoginPage.vue
@@ -1,16 +1,12 @@
 <template>
   <q-page class="full-width full-height bg-blue-3">
-    <div class="absolute row no-wrap full-width full-height flex flex-center q-gutter-md">
-      <div class="div-with-bg"></div>
-      <div class="div-with-bg"></div>
-      <div class="div-with-bg"></div>
+    <div class="absolute row justify-center no-wrap full-width full-height q-gutter-md">
+      <div class="div-with-bg col"></div>
+      <div class="div-with-bg col"></div>
+      <div class="div-with-bg col"></div>
     </div>
-    <div class="absolute row no-wrap full-width full-height q-gutter-md">
-      <q-card
-        style="min-width: 500px; top: 150px; right: 150px"
-        class="absolute"
-        bordered="false"
-      >
+    <div class="absolute full-height full-width q-gutter-md flex flex-center">
+      <q-card style="min-width: 500px; width: 40%; margin-top: -100px">
         <q-card-section class="bg-primary text-white q-pa-sm">
           <div class="text-h7">Sign In</div>
         </q-card-section>
@@ -22,7 +18,7 @@
             style="width: 300px"
           >
             <q-input dense outlined v-model="username" type="text" label="UserName" />
-            <q-input dense outlined v-model="password" type="password" label="Password" />
+            <InputPassword v-model="password"></InputPassword>
           </q-form>
         </q-card-section>
 
@@ -39,6 +35,7 @@
 </template>
 
 <script setup>
+import InputPassword from "src/components/form/InputPassword.vue";
 import { useAppConfig } from "src/stores/useAppConfig";
 import { useRouter } from "vue-router";
 
@@ -57,12 +54,12 @@ const onRegister = () => {
 
 <style scoped>
 .div-with-bg {
-  width: 450px; /* 根据需要调整宽度 */
-  height: 450px; /* 根据需要调整高度 */
+  width: 100px;
+  aspect-ratio: 1 / 1;
   background-image: url("/icons/logo.svg"); /* 替换为你的图片URL */
-  background-size: 450px 450px; /* 设置背景图片的具体尺寸 */
   background-position: center; /* 居中显示背景图片 */
-  background-repeat: no-repeat; /* 确保背景图片不会重复 */
+  background-repeat: no-repeat;
+  background-size: calc(100% * 0.8);
   opacity: 1;
 }
 </style>
