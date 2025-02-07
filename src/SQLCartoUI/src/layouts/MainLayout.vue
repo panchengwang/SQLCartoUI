db334c38a8d3064d913fdab95f00ef1e2b3a9543
pcwang （麓山老将） <wang_wang_lao@163.com>
2025-02-07 13:42:01 +0800

    Update MainLayout.vue

--- src/SQLCartoUI/src/layouts/MainLayout.vue
+++ src/SQLCartoUI/src/layouts/MainLayout.vue
@@ -2,19 +2,82 @@
   <q-layout view="hHh lpR fFf" class="full-height">
     <q-header bordered height-hint="32px">
       <q-toolbar style="min-height: 36px">
-        <q-btn flat dense round icon="menu" aria-label="Menu" />
+        <q-btn
+          size="sm"
+          color="white"
+          dense
+          round
+          icon="img:/icons/logo.svg"
+          aria-label="Menu"
+        />
 
         <q-toolbar-title> Welcome to PCWang's GIS World. </q-toolbar-title>
-
-        <q-btn
+        <q-btn-dropdown
           dense
           flat
           color="white"
-          icon-right="account_circle"
-          size="md"
-          label="Sign In"
+          :label="appConfig.$state.username.trim()"
+          icon="account_circle"
           no-caps
-        />
+          v-show="
+            appConfig.$state.token.trim() !== '' &&
+            appConfig.$state.username.trim() !== ''
+          "
+        >
+          <div class="row no-wrap q-pa-md" style="z-index: 1000">
+            <div class="column">
+              <div class="text-h6 q-mb-md">Settings</div>
+              <q-toggle v-model="mobileData" label="Use Mobile Data" />
+              <q-toggle v-model="bluetooth" label="Bluetooth" />
+            </div>
+
+            <q-separator vertical inset class="q-mx-lg" />
+
+            <div class="column items-center">
+              <q-avatar size="72px">
+                <img src="/icons/pcwang.png" />
+              </q-avatar>
+
+              <div class="text-subtitle1 q-mt-md q-mb-xs">
+                {{ appConfig.$state.username.trim() }}
+              </div>
+
+              <q-btn
+                color="primary"
+                label="Sign Out"
+                push
+                size="md"
+                v-close-popup
+                @click="onLogout"
+                no-caps
+              />
+            </div>
+          </div>
+        </q-btn-dropdown>
+        <!-- <q-btn-dropdown
+          dense
+          flat
+          color="white"
+          icon="account_circle"
+          size="md"
+          :label="appConfig.$state.username.trim()"
+          no-caps
+          v-show="
+            appConfig.$state.token.trim() !== '' &&
+            appConfig.$state.username.trim() !== ''
+          "
+        >
+          <q-list>
+            <q-item v-ripple clickable v-close-popup>
+              <q-item-section avatar>
+                <q-avatar icon="check" size="md"> </q-avatar>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>asfsf</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown> -->
         <!-- <div>Quasar v{{ $q.version }}</div> -->
       </q-toolbar>
     </q-header>
@@ -32,11 +95,21 @@
 </template>
 
 <script setup>
-// import { ref } from "vue";
+import { useAppConfig } from "src/stores/useAppConfig";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
 
-// const leftDrawerOpen = ref(false);
+const appConfig = useAppConfig();
+const router = useRouter();
 
-// function toggleLeftDrawer() {
-//   leftDrawerOpen.value = !leftDrawerOpen.value;
-// }
+onMounted(() => {
+  if (appConfig.hasLogin) {
+    router.replace({ name: "application" });
+  }
+});
+
+const onLogout = () => {
+  appConfig.setAccountInformation("", "");
+  router.replace({ name: "login" });
+};
 </script>
