c381ac472f1061ef8c4cbef3e717d9419960e20a
pcwang （麓山老将） <wang_wang_lao@163.com>
2025-02-07 22:17:06 +0800

    Update RegisterPage.vue

--- src/SQLCartoUI/src/pages/RegisterPage.vue
+++ src/SQLCartoUI/src/pages/RegisterPage.vue
@@ -1,5 +1,52 @@
 <template>
-  <q-page class="full-width full-height bg-blue-3 flex flex-center"> register </q-page>
+  <q-page class="item-center">
+    <div class="fit flex justify-center q-pt-xl">
+      <q-form
+        class="q-gutter-md"
+        style="min-width: 500px; width: 50%"
+        @submit="onRegister"
+      >
+        <InputUserName v-model="username"> </InputUserName>
+        <InputPassword dense outlined v-model="password" hide-bottom-space />
+        <InputPassword
+          label="Please input password again."
+          v-model="password2"
+          :rules="[
+            (val) =>
+              password === password2 ||
+              'The password does not match the one entered previously.',
+          ]"
+        ></InputPassword>
+
+        <InputCaptcha v-model="captcha"></InputCaptcha>
+        <div>
+          <q-btn dense label="Submit" type="submit" color="primary" no-caps />
+          <q-btn
+            dense
+            label="Reset"
+            type="reset"
+            color="primary"
+            flat
+            class="q-ml-sm"
+            no-caps
+          />
+        </div>
+      </q-form>
+    </div>
+  </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+import InputCaptcha from "src/components/form/InputCaptcha.vue";
+import InputPassword from "src/components/form/InputPassword.vue";
+import InputUserName from "src/components/form/InputUserName.vue";
+import { ref } from "vue";
+const username = ref("wang_wang_lao@163.com");
+const password = ref("@Ww111111");
+const password2 = ref("@Ww111111");
+const captcha = ref("");
+
+const onRegister = () => {
+  console.log("register");
+};
+</script>
