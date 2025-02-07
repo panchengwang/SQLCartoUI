6f5f1a53784fd9c8b2024b880316f2cf338e0cf6
pcwang （麓山老将） <wang_wang_lao@163.com>
2025-02-07 23:10:26 +0800

    Update ApplicationPage.vue

--- src/SQLCartoUI/src/pages/ApplicationPage.vue
+++ src/SQLCartoUI/src/pages/ApplicationPage.vue
@@ -3,14 +3,8 @@
     <q-splitter v-model="splitterModel" class="full-height" unit="%" :limits="limits">
       <template v-slot:before>
         <div class="q-pa-none full-height">
-          <div>
-            <!-- <div class="text-h4 q-mb-md">Before</div>
-            <div v-for="n in 20" :key="n" class="q-my-md">
-              {{ n }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-              praesentium cumque magnam odio iure quidem, quod illum numquam possimus
-              obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In,
-              libero.
-            </div> -->
+          <div class="full-width full-height">
+            <!-- <MapControl class="full-width full-height"></MapControl> -->
           </div>
         </div>
       </template>
