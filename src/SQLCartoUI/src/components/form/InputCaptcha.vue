7d5368d104e26e18d164529b46b29f1e7a17b366
pcwang （麓山老将） <wang_wang_lao@163.com>
2025-02-09 20:05:44 +0800

    Update InputCaptcha.vue

--- src/SQLCartoUI/src/components/form/InputCaptcha.vue
+++ src/SQLCartoUI/src/components/form/InputCaptcha.vue
@@ -7,15 +7,60 @@
     label="Input Captcha"
     :rules="[
       (val) =>
-        (val && val.length === 8) ||
-        'The password does not match the one entered previously.',
+        (val && val.length >= props.minLength && val.length <= props.maxLength) ||
+        'The captcha is invalid: length must be greater than or equals to  ' +
+          props.minLength +
+          ' and be less than or equals to ' +
+          props.maxLength +
+          ' .',
     ]"
   >
     <template v-slot:append>
-      <q-btn dense size="sm" round color="primary" icon="code" no-caps>
+      <q-btn
+        dense
+        size="sm"
+        round
+        color="primary"
+        icon="code"
+        no-caps
+        @click="onGetCaptcha"
+      >
         <q-tooltip class="bg-accent" anchor="top middle">Get captcha</q-tooltip>
       </q-btn>
     </template>
   </q-input>
 </template>
-<script setup></script>
+<script setup>
+import { useQuasar } from "quasar";
+import SQLCartoDatabase from "src/net/SQLCartoDatabase.js";
+import { toRefs } from "vue";
+
+const $q = useQuasar();
+const props = defineProps({
+  minLength: {
+    type: Number,
+    default: 6,
+  },
+  maxLength: {
+    type: Number,
+    default: 6,
+  },
+  username: {
+    type: String,
+    default: "",
+  },
+});
+
+const { username } = toRefs(props);
+
+const onGetCaptcha = () => {
+  const db = new SQLCartoDatabase({});
+  db.userGetCaptcha(username.value, (response) => {
+    $q.notify({
+      color: "purple",
+      position: "top",
+      message: response.message,
+    });
+  });
+};
+</script>
